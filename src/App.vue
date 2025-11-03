76750d70aaecc5a09becb1d66ae4581866f56780
Jonnynho <ricenice1221@gmail.com>
2025-11-03 15:08:59 -0300

    launch commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue';
+import Cabecalho from './components/Cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+import Resultado from './components/Resultado.vue';
+
+const estado = reactive({
+  numA: 0,
+  numB: 0,
+  operacao: '',
+})
+
+const calculo = () => {
+  const {operacao, numA, numB} = estado;
+  switch(operacao){
+    case '+': 
+      return numA + numB;
+    case '-':
+      return numA - numB;
+    case '*':
+      return numA * numB;
+    case '/':
+      return numB !== 0 ? numA / numB : 'Erro: divisão por zero';
+    default:
+      return 'Selecione uma operação'
+  }
+}
+
+
+const atualizar = ({ numA, numB, operacao }) => {
+  estado.numA = numA
+  estado.numB = numB
+  estado.operacao = operacao
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Cabecalho />
+  <Formulario @atualizar="atualizar" />
+  <Resultado :calculo="calculo()" />
 </template>
 
 <style scoped></style>
